3570c12e8d4501330c508b4dbba47f640568a1de
Hung-qwerty <viethun.mc2023@gmail.com>
2026-03-24 12:45:06 +0700

    Change DB connection URL to new server IP
    Updated database connection URL to use new server IP.

--- src/main/java/com/quanlysinhvien/util/DBConnection.java
+++ src/main/java/com/quanlysinhvien/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:sqlserver://localhost\\SQLEXPRESS:1433"
+            "jdbc:sqlserver://192.168.1.32:1433"
                     + ";databaseName=QuanLySinhVien"
                     + ";encrypt=true"
                     + ";trustServerCertificate=true"
